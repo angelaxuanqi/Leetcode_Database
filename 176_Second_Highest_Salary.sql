ea9d09e2d1936671046670bb029a5b41cd967419
anqi xu <angelaxuanqi@gmail.com>
2022-03-01 01:11:54 -0500

    rename

--- 176_Second_Highest_Salary.sql
+++ 176_Second_Highest_Salary.sql
@@ -67,7 +67,8 @@ Language: **MySQL**
 
 ```mysql
 # Write your MySQL query statement below
-select distinct salary as SecondHighestSalary from Employee
-order by salary desc 
-limit 1 offset 1
+select 
+    (select distinct salary as SecondHighestSalary from Employee
+        order by salary desc 
+        limit 1 offset 1) as SecondHighestSalary
 ```
