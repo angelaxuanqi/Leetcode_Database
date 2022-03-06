41b3e5353a233f102702b728e56e0c14f9cd249c
anqi xu <angelaxuanqi@gmail.com>
2022-03-06 01:19:23 -0500

    DELETE statement

---
+++ 196_Delete_Duplicate_Emails.sql
@@ -0,0 +1,61 @@
+### [196\. Delete Duplicate Emails](https://leetcode.com/problems/delete-duplicate-emails/)
+
+Difficulty: **Easy**  
+
+Related Topics: [Database](https://leetcode.com/tag/database/)
+
+
+Table: `Person`
+
+```
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+```
+
+Write an SQL query to **delete** all the duplicate emails, keeping only one unique email with the smallest `id`. Note that you are supposed to write a `DELETE` statement and not a `SELECT` one.
+
+Return the result table in **any order**.
+
+The query result format is in the following example.
+
+**Example 1:**
+
+```
+Input: 
+Person table:
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Output: 
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+Explanation: john@example.com is repeated two times. We keep the row with the smallest Id = 1.
+```
+
+
+#### Solution
+
+Language: **MySQL**
+
+```mysql
+# Write your MySQL query statement below
+DELETE A
+FROM Person A
+    , Person B
+WHERE A.email = B.email
+AND A.id > B.id
+```
